a4bae35a15557da5c2652045cbe8991f307095d5
ndl-lab <54262546+ndl-lab-staff@users.noreply.github.com>
2023-08-24 09:59:31 +0900

    Update process.py

--- src/ndl_kotenseki_layout/tools/process.py
+++ src/ndl_kotenseki_layout/tools/process.py
@@ -49,7 +49,7 @@ class LayoutDetector:
         #self.colors = generate_class_colors(len(self.classes))
 
     def load(self, config: str, checkpoint: str, device: str):
-        self.model = init_detector(config, checkpoint, device)
+        self.model = init_detector(config,checkpoint,None,device)
 
     def predict(self, img_path: str):
         return inference_detector(self.model, img_path)
